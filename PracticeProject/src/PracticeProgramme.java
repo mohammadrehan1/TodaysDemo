54678f770b93bdcfc032562bc5e2aabb4cfe0d4c
mohammadrehan1 <71365783+mohammadrehan1@users.noreply.github.com>
2020-09-17 09:17:00 +0530

    Update PracticeProgramme.java
    Now we have added m1() method

--- PracticeProject/src/PracticeProgramme.java
+++ PracticeProject/src/PracticeProgramme.java
@@ -1,6 +1,10 @@
 
 public class PracticeProgramme {
 
+	public void m1(){
+	    System.out.println("Just now m1() method added in Gits");
+	}
+	
 	public static void main(String[] args) {
 	System.out.println("This is inside main method");
 	}
